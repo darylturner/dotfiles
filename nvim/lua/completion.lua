3ee1c6fffdded2bf3efc65c6c007ed32f27efd7c
Daryl Turner <daryl.turner@global.ntt>
2022-06-28 12:53:59 +0100

    update(cmp): change keymaps

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -1,5 +1,6 @@
 local cmp = require "cmp"
 local luasnip = require "luasnip"
+-- local types = require "luasnip.util.types"
 
 require("luasnip.loaders.from_snipmate").lazy_load({ include = {"jinja", "python", "go"} })
 
@@ -10,14 +11,14 @@ cmp.setup {
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm {
+        ["<c-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<c-f>"] = cmp.mapping.scroll_docs(4),
+        ["<c-space>"] = cmp.mapping.complete(),
+        ["<c-y>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        ["<c-n>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
@@ -26,7 +27,7 @@ cmp.setup {
                 fallback()
             end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        ["<c-p>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
